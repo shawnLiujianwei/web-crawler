88d4a5f8562cca75d5f39152e64fcc1c6a780278
Shawn Liu <shawn.liu@dotter.me>
2015-05-18 15:29:56 +0800

    ensure can kill process by port

--- components/utils/dotterUtil.js
+++ components/utils/dotterUtil.js
@@ -9,7 +9,8 @@ exports.freePort = function (port) {
     return new Promise(function (resolve, reject) {
         logger.info("freeing up port " + port + " if still in use");
         //var command = "kill -9 `lsof -n -iTCP:" + port + " | grep LISTEN | awk '{print $2}'`";
-        var command = "kill $(lsof -t -i:" + port + ")";
+        // var command = "kill -9 $(lsof -t -i:" + port + ")";
+        var command = "fuser -k " + port + "/tcp";
         if (os.platform().indexOf("win") === -1) {
             Process.exec(command, function (err, data) {
                 if (err) {
